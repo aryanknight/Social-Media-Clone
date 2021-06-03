61ecd116f4bb9939584c671810566fcf32db6baa
Aryan <aryanknight12@gmail.com>
2021-06-03 20:45:01 +0530

    V2

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,24 +1,43 @@
-import { Container,Grow ,Grid} from '@material-ui/core'
-import React from 'react'
+import { Container,Grow ,Grid , useMediaQuery} from '@material-ui/core';
+import { createMuiTheme, ThemeProvider, useTheme } from '@material-ui/core/styles';
+import React , {useState , useEffect} from 'react'
 import Form from '../Form/Form';
 import Posts from '../Posts/Posts';
 import './Home.css'
 
+
+const theme = createMuiTheme();
+
 export default function Home() {
     const userInfo=JSON.parse(localStorage.getItem('User'));
+    const [gridDirn , setgridDirn]=useState('row-reverse');
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('md'));
+    const matches_1 = useMediaQuery(theme.breakpoints.up('lg'));
+    
+    useEffect(()=>{
+        if(matches || matches_1){
+            setgridDirn('row');
+        }else{
+            setgridDirn('row-reverse');
+        }
+    },[matches]);
+    
     return (
     <Grow in>
         <main>
+        <ThemeProvider theme={theme}>
             <Container >
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={8}>
-                        <Posts/>
-                    </Grid>
+                <Grid container spacing={3} direction='row-reverse' >
                     <Grid item xs={12} md={4}>
                         <Form/>
                     </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Posts/>
+                    </Grid>
                 </Grid>
             </Container>
+        </ThemeProvider>    
        </main>
     </Grow>
     );
--- src/Component/Posts/Posts.jsx
+++ src/Component/Posts/Posts.jsx
@@ -1,18 +1,22 @@
 import React,{useEffect,useState} from 'react'
-import {Grid} from '@material-ui/core';
+import {Grid,LinearProgress , CircularProgress} from '@material-ui/core';
 import Post from './Post';
 import { useSelector, useDispatch } from "react-redux";
 import {getPosts} from '../../API/api';
 import {getPost} from '../../Actions/postActions';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 export default function Posts() {
     
-    const posts=useSelector((state)=>state.postReducer);    
+    const posts=useSelector((state)=>state.postReducer);   
+    const [progress,setProgress]=useState(true); 
 
     const dispatch=useDispatch();
     useEffect(()=>{
         const fetchingData = async ()=>{
             const {data} =await getPosts();
+            setProgress(false);
             dispatch(getPost(data));
         };
         fetchingData();
@@ -20,12 +24,17 @@ export default function Posts() {
 
     return (
         <Grid container spacing={3}>
-
+            {progress ?
+            <Grid item xs={12} md={12} lg={12}>
+                 <LinearProgress color='secondary' />   
+            </Grid>  : null 
+            }
             {posts.map((post)=>(
                 <Grid item key={post._id} xs={12} md={6}>
                     <Post post={post}/>
                 </Grid>
             ))}
+            
             
         </Grid>
     );
